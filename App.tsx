5f0cf5ca514585b4f819562efe77ba8858f6668e
camwhite <whiteceric@gmail.com>
2021-10-29 23:22:14 -0400

    Add navigation and menu buttons

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './components/HomeScreen';
 
 export default function App() {
   return (
-    <HomeScreen />
+    <NavigationContainer>
+      <HomeScreen />
+    </NavigationContainer>
   );
 }
 
--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { StatusBar } from 'expo-status-bar';
 
 export const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello World! From HomeScreen</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity
+        activeOpacity={0.7}
+        style={styles.menuButton}
+        onPress={() => console.log('Python REPL')}
+      >
+        <Text style={styles.menuButtonText}>Python REPL</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        style={styles.menuButton}
+        onPress={() => console.log('Settings')}
+      >
+        <Text style={styles.menuButtonText}>Settings</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        style={styles.menuButton}
+        onPress={() => console.log('About')}
+      >
+        <Text style={styles.menuButtonText}>About</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -14,8 +33,20 @@ export const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#745C97',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  menuButton: {
+    backgroundColor: '#39375B',
+    width: '40%',
+    margin: 20,
+    padding: 10,
+    borderRadius: 10,
+  },
+  menuButtonText: {
+    textAlign: 'center',
+    fontSize: 56,
+    color: '#F5B0CB',
   },
 });
