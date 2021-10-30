993d256dd97479ad011fde8cb9c7813c3e179fba
camwhite <whiteceric@gmail.com>
2021-10-30 19:17:31 -0400

    Add drawer items

--- ipad-repl-app/App.tsx
+++ ipad-repl-app/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 import { HomeScreen } from './components/HomeScreen';
 import { REPLScreen } from './components/REPLScreen';
 
@@ -20,19 +20,24 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator screenOptions={{ headerShown: false }}>
+      <Drawer.Navigator
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+        screenOptions={{ headerShown: false }}
+      >
         {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
         <Drawer.Screen name="REPLScreen" component={REPLScreen} options={
-          { 
+          {
             headerShown: true,
-            title: 'Python3',
+            headerTitle: 'Python3',
+            title: 'REPL',
+            headerStyle: styles.headerContainer,
             headerRight: () => (
               <TouchableOpacity
                 activeOpacity={0.7}
                 style={styles.headerSettingsButton}
                 onPress={() => alert('This is a button!')}
               >
-                <Image 
+                <Image
                   style={styles.settingsImage}
                   source={settingsImage}
                 />
@@ -45,12 +50,43 @@ export default function App() {
   );
 }
 
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItem
+        label='iREPL'
+        style={styles.drawerTitleView}
+        labelStyle={styles.drawerTitle}
+        onPress={() => { }}
+      />
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label='Settings'
+        onPress={() => console.log('Settings')}
+      />
+    </DrawerContentScrollView>
+  );
+}
+
 const styles = StyleSheet.create({
+  headerContainer: {
+    backgroundColor: '#f00',
+  },
   headerSettingsButton: {
+    marginRight: 10,
   },
   settingsImage: {
     width: 30,
     height: 30,
     tintColor: "blue",
+  },
+  drawerTitleView: {
+    marginBottom: '20%',
+  },
+  drawerTitle: {
+    fontFamily: 'Courier New',
+    fontWeight: 'bold',
+    fontSize: 48,
+    textAlign: 'center',
   },
 });
